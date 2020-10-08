de872ea2103a55692253225021e35b0e50ca6d99
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2020-10-08 17:00:11 +0200

    Add link to homepage

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'typeface-roboto';
 
+import Link from "@material-ui/core/Link";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -36,7 +37,7 @@ export default function ButtonAppBar(props) {
 
       	  {/* Name of the page */}
           <Typography variant="h6" className={classes.title}>
-            RecordLinkage ANNOTATOR
+            <Link href="/" variant="inherit" color="inherit">RecordLinkage ANNOTATOR</Link>
           </Typography>
 
           {props.reviewState === "never" &&
